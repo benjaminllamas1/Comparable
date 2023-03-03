9b1245d9f363faf396f95166b763d8372ce8a09b
Ben <Ben@Deborahs-iMac-2.local>
2023-03-02 23:48:12 -0700

    Initial commit

---
+++ src/main/java/org/example/comparable/Color.java
@@ -0,0 +1,5 @@
+package org.example.comparable;
+
+public enum Color {
+    RED, GREEN, YELLOW
+}
---
+++ src/main/java/org/example/comparable/Driver.java
@@ -0,0 +1,30 @@
+package org.example.comparable;
+
+import java.util.*;
+public class Driver {
+
+    public static void main(String[] args) {
+
+        Apple apple1 = new Apple(Size.LARGE, Color.GREEN, "Granny Smith");
+        Apple apple2 = new Apple(Size.MEDIUM, Color.RED, "Fuji");
+        Apple apple3 = new Apple(Size.LARGE, Color.RED, "Gala");
+        Apple apple4 = new Apple(Size.MEDIUM, Color.YELLOW, "Golden Delicious");
+        Apple apple5 = new Apple(Size.SMALL, Color.RED, "McIntosh");
+        Apple apple6 = new Apple(Size.SMALL, Color.GREEN, "Green McIntosh");
+        Apple apple7 = new Apple(Size.XLARGE, Color.GREEN, "Green McIntosh");
+
+        List<Apple> apples = new ArrayList<>();
+        apples.add(apple1);
+        apples.add(apple2);
+        apples.add(apple3);
+        apples.add(apple4);
+        apples.add(apple5);
+        apples.add(apple6);
+        apples.add(apple7);
+
+        apples.forEach(System.out::println);
+        Collections.sort(apples);
+        apples.forEach(System.out::println);
+
+    }
+}
